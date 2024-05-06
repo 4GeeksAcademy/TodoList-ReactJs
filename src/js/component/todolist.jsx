c0b9300771a1ef8d2e310ac364ed3890351cd2c4
Beethove1 <96740916+Beethove1@users.noreply.github.com>
2024-05-06 17:36:28 +0000

    ToDoList_Last Commit

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -6,21 +6,26 @@ import InputGroup from 'react-bootstrap/InputGroup';
 function ToDoList() {
     const [tasks, setTask]=useState([]);
     const [newTask, setNewTask]=useState([]);
-    const [isTearing, setIsTearing]=useState(false)
-    const [isCompleted, setIscompleted]=useState(false)
-
+    const [isTearing, setIsTearing]=useState(false);
+    const [isCompleted, setIscompleted]=useState(false);
+    
 
     function handleInputChange(event){
       setNewTask(event.target.value);
     }
     function AddTask(){
       if(newTask.trim()!==""){
-        setTask(t=>[...t, newTask]);
+        const newTaskObject = {
+          description: newTask,
+          isCompleted: false
+        };
+        setTask(t=>[...t, newTaskObject]);
         setNewTask("");
       };
       
     };
 
+    
     const deleteAllTasks = () => {
       setIsTearing(true);
       setTimeout(() => {
@@ -33,8 +38,17 @@ function ToDoList() {
       const updateTasks=tasks.filter((_, i)=> i !==index);
       setTask(updateTasks);
     }
-   
+
     
+    function MarkAsCompleted(index) {
+      const updatedTasks = tasks.map((task, i) => {
+        if (i === index) {
+          return {...task, isCompleted: !task.isCompleted};
+        }
+        return task;
+      });
+      setTask(updatedTasks);
+    }
    
     const completedTasksCount = tasks.filter((task) => task.isCompleted).length;
   return (
@@ -78,22 +92,16 @@ function ToDoList() {
         
         >
             <span className='task'>
-                {task}
+                {task.description}
+                
+                
             </span>
             <Button className='add' variant="outline-success" 
-            onClick={() => MasrkasCompleted(index)}>
-            {tasks.isCompleted ? "Mark as Uncompleted" : "Mark as Completed"}
-             
+            onClick={() => MarkAsCompleted(index)}>
+            {task.isCompleted ? "Mark as Uncompleted" : "Mark as Completed"}
              
 
              </Button>
-
-             <Button className='update'onClick={()=>{}} variant="warning">
-             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg>
-          </Button>
 
             <Button className='delete'onClick={()=>DeleteTask(index)} variant="danger">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
